fc1360a3b7ad87dd2702f6e3e7ef16f9690a5889
wenhang <107180338+wenhang-cheil@users.noreply.github.com>
2024-11-15 15:55:31 +0800

    react-practice

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
